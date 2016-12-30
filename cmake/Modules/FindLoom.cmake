02974c20b0fa5c89e03f5eda4b627a00abd9cb1e
Jonathan Anderson <jonathan.anderson@ieee.org>
2016-12-29 22:36:34 -0330

    Refactor Loom finding out into FindLoom.cmake.

---
+++ cmake/Modules/FindLoom.cmake
@@ -0,0 +1,54 @@
+# Copyright (c) 2016 Jonathan Anderson
+# All rights reserved.
+#
+# This software was developed by BAE Systems, the University of Cambridge
+# Computer Laboratory, and Memorial University under DARPA/AFRL contract
+# FA8650-15-C-7558 ("CADETS"), as part of the DARPA Transparent Computing
+# (TC) research program.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+
+find_path(LOOM_INCLUDE_DIR
+	loom/Instrumenter.hh
+	PATHS ${LOOM_PREFIX}/include /usr/local/include /usr/include
+)
+
+find_library(LOOM_LIBRARY
+	NAMES LLVMLoom
+	PATHS ${LOOM_PREFIX}/lib /usr/local/lib /usr/lib
+)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Loom
+	"Unable to find Loom: try setting LOOM_PREFIX?"
+	LOOM_LIBRARY LOOM_INCLUDE_DIR
+)
+
+mark_as_advanced(LOOM_INCLUDE_DIR LOOM_LIBRARY)
+
+#if(NOT LOOM_INCLUDE_DIR OR NOT LOOM_LIBRARY)
+#	message(FATAL_ERROR "can't find Loom: try setting LOOM_PREFIX?")
+#endif()
+
+set(LOOM_LIBRARIES ${LOOM_LIBRARY})
+set(LOOM_INCLUDE_DIRS ${LOOM_INCLUDE_DIR})
